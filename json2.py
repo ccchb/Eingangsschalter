02e2fda2dee415b6e30ccead702bed5ba04eee11
Yannik Schädler <y.schaedler@uni-bremen.de>
2017-12-05 22:08:22 +0100

    haufenweise optimierungen

--- json2.py
+++ json2.py
@@ -7,38 +7,16 @@ data = {1:"asdf"}
 wert_des_schalters = False
 
 while True:
-    time.sleep(10)  # Anzahl der sekunden, die gewartet wird
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    wert_des_schalters = GPIO.input(18)
-
-
-    if wert_des_schalters == False:
-        data = {1: wert_des_schalters}
-        with open('data.txt', 'w') as outfile:
-            json.dump(data, outfile)
-
-    if wert_des_schalters == True: 
-        data = {1: wert_des_schalters}
-        with open('data.txt', 'w') as outfile:
-            json.dump(data, outfile)
-
-
 #/* Sphinx Vorschlag */
-'''
-
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    
-    while true:
-    sleep(10);
-    
-    if wert_des_schalters != GPIO:Input(pin_number)  # das ist gut
-        sleep(1); // Contact bounce protection
-        wert_des_schalters = GPIO.Input(pin_number)
-        data = {1:wert_des_schalters}
-        with open('data.txt', 'w') as outfile:
-            json.dump(data, outfile)
+    while True:
+        time.sleep(1)
+        if wert_des_schalters !=  GPIO.input(18) : # das ist gut
+            time.sleep(1) # // Contact bounce protection
+            print("wert wurde geaendert")
+            wert_des_schalters = GPIO.input(pin_number)
+            data = {1:wert_des_schalters}
+            with open('data.txt', 'w') as outfile:
+                json.dump(data, outfile)
 
-'''
